7594b0c67199c4e3ef2a4323ee1f65883922fa7b
yuragms <yura.gms@gmail.com>
2021-07-26 16:23:31 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+import axios from "axios";
+import photoCardsTemplates from './templates/photoCards.hbs';
+
+const gallery = document.querySelector('.gallery');
+
+let art;
+
+
+
+axios.get('https://pixabay.com/api/?key=22652626-da7dc8e3a4ffdfdaea60a5cb5&q=cat&image_type=photo&orientation=horizontal&safesearch=true')
+  .then(res => {
+    
+    art = res.data.hits;
+    console.log(art);
+  });
+  gallery.innerHTML = photoCardsTemplates(art);
